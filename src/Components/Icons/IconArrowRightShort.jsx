4e028605859336ca949f06af6fb305bb41125e86
Nidhi Singh <nidhi4upooja@gmail.com>
2025-11-25 15:10:15 +0530

    added pizza Image

---
+++ src/Components/Icons/IconArrowRightShort.jsx
@@ -0,0 +1,16 @@
+export default function IconArrowRightShort(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="1em"
+      height="1em"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="m12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"
+      ></path>
+    </svg>
+  );
+}
