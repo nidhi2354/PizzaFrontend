11fb07e549170912a03ec1ebfb8ecb1b487edd87
Nidhi Singh <nidhi4upooja@gmail.com>
2025-12-04 21:39:16 +0530

    Added presentation layer for add product component

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,19 @@ import Signup from "./Pages/Auth/Signup.jsx";
 import Login from "./Pages/Auth/Login.jsx";
 import NotFound from "./Pages/Auth/NotFound.jsx";
 
+import Denied from "./Pages/Auth/Denied.jsx";
+import AddProduct from "./Pages/Admin/Addproduct.jsx";
+
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/denied" element={<Denied />} />
         <Route path="/auth/signup" element={<Signup />} />
         <Route path="/auth/login" element={<Login />} />
         <Route path="*" element={<NotFound />} />
+        <Route path="/admin/addProduct" element={<AddProduct />} />
       </Routes>
     </>
   );
